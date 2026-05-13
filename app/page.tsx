f269405544f39828705c8021c32fc26e23542ecb
Michelle Livingston <msalivingston@gmail.com>
2026-05-13 15:44:48 -0600

    Initial FlipFlocks app

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        padding: "60px",
+        fontFamily: "Arial",
+        backgroundColor: "#f3f0e8",
+        minHeight: "100vh",
+      }}
+    >
+      <h1 style={{ fontSize: "48px" }}>
+        FlipFlocks
+      </h1>
+
+      <p style={{ fontSize: "20px" }}>
+        Poultry listings that don’t suck.
+      </p>
+    </main>
+  );
+}
